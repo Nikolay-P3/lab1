defa891a373d92c13ee1bb6e70bdb0cc075c0b06
Nicholas <Nicholas@DESKTOP-4UNSJUC>
2019-03-10 19:08:05 +0300

    2

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿//Задание 14.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1
+namespace Laboratory_work__1__task_14_
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var res = SquareSum(1, 2);
+            Console.WriteLine("Ответ: {0}", res); //Выведет 9.
+            Console.ReadKey();
+        }
+
+        static double SquareSum(double x, double y)
+        {
+            double sum = x + y;
+            double result = Math.Pow(sum, 2);
+
+            return result;
         }
     }
 }
